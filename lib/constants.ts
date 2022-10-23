24a1d07137a8210af29a1d4bd889cc33a53cc00f
Simon Warta <simon@warta.it>
2022-10-23 22:05:16 +0200

    Update numberOfRewardedSubmissions to 6

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 import { assert } from "@cosmjs/utils";
 
-export const numberOfRewardedSubmissions = 5;
+export const numberOfRewardedSubmissions = 6;
 
 assert(process.env.NEXT_PUBLIC_NOIS_CONTRACT, "NEXT_PUBLIC_NOIS_CONTRACT must be set");
 export const noisOracleAddress = process.env.NEXT_PUBLIC_NOIS_CONTRACT;
