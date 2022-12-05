470cb806d95f2184f798b178c612884b6c3fac88
Simon Warta <simon@warta.it>
2022-12-05 22:56:25 +0100

    Implement Home navigation

--- pages/rounds/[round].tsx
+++ pages/rounds/[round].tsx
@@ -1,11 +1,23 @@
-import { Container, Heading, Skeleton, Stack } from "@chakra-ui/react";
+import {
+  Code,
+  Container,
+  Heading,
+  HStack,
+  IconButton,
+  Skeleton,
+  Spacer,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { assert } from "@cosmjs/utils";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 import { useContext, useEffect, useState } from "react";
 
 import { GlobalContext } from "../../lib/GlobalState";
 import { VerifiedBeacon } from "../../lib/beacons";
+import Link from "next/link";
 
 const Round: NextPage = () => {
   const [loading, setLoading] = useState(false);
@@ -36,6 +48,18 @@ const Round: NextPage = () => {
 
   return (
     <Container maxW="800px" paddingTop="5px" paddingBottom="25px">
+      <HStack paddingTop="20px" paddingBottom="20px" direction="row">
+        <Link href="/">
+          <IconButton
+            colorScheme="gray"
+            aria-label="Home"
+            size="md"
+            icon={<ArrowBackIcon />}
+          />
+        </Link>
+        <Spacer />
+      </HStack>
+
       {loading && (
         <Stack spacing="25px">
           <Skeleton height="150px" />
@@ -47,7 +71,19 @@ const Round: NextPage = () => {
       )}
 
       <Heading size="lg">Round #{round}</Heading>
-      <div>{beacon ? JSON.stringify(beacon, null, 2) : <>Not found</>}</div>
+      <div>
+        {beacon ? (
+          <Text>
+            Published: {beacon.published.toUTCString()}
+            <br />
+            Verified: {beacon.verified.toUTCString()} ({beacon.diff.toFixed(1)}s)
+            <br />
+            <Code>{beacon.randomness}</Code>
+          </Text>
+        ) : (
+          <>Not found</>
+        )}
+      </div>
     </Container>
   );
 };
