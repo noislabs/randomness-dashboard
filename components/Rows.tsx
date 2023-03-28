faaa7dfcd769ccd250d1ce591dcffb4baf3ce0f1
Simon Warta <simon@warta.it>
2023-03-28 21:16:19 +0200

    Add empty state

--- components/Rows.tsx
+++ components/Rows.tsx
@@ -1,3 +1,4 @@
+import { Text } from "@chakra-ui/react";
 import { maxBeaconRows } from "../lib/settings";
 import { DisplayBeacon, Row } from "./Row";
 
@@ -10,16 +11,20 @@ interface Props {
 export function Rows({ beacons, highlightedAddress, onHighlightAddress }: Props): JSX.Element {
   return (
     <>
-      {beacons.slice(0, maxBeaconRows).map((beacon) => {
-        return (
-          <Row
-            key={beacon.round}
-            beacon={beacon}
-            highlightedAddress={highlightedAddress}
-            onHighlightAddress={onHighlightAddress}
-          />
-        );
-      })}
+      {beacons.length ? (
+        beacons
+          .slice(0, maxBeaconRows)
+          .map((beacon) => (
+            <Row
+              key={beacon.round}
+              beacon={beacon}
+              highlightedAddress={highlightedAddress}
+              onHighlightAddress={onHighlightAddress}
+            />
+          ))
+      ) : (
+        <Text>No beacons available</Text>
+      )}
     </>
   );
 }
